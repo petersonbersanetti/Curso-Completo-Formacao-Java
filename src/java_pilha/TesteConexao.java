38e568e3ea51cc8f933a9826a6ea88dd6d3a2b0b
Peterson Bersanetti <peterson.bersanetti@gmail.com>
2022-11-13 23:05:33 -0300

    Aprendendo a forma atual de utilizar try catch close
    Antigamente usava-se finally após o catch, atualmente é implementado na classe AutoCloseable e criado o método close() com isso podemos criar uma conexão dentro do próprio try (Conexao conexao = new Conexao() e lançar a exceção no catch

--- src/java_pilha/TesteConexao.java
+++ src/java_pilha/TesteConexao.java
@@ -3,17 +3,30 @@ package java_pilha;
 public class TesteConexao {
 
 	public static void main(String[] args) {
-		Conexao con = null;
-		try {
-			con = new Conexao();
-			con.leDados();
-			con.fecha();
-		} catch (IllegalStateException ex) {
+
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
+
+		} catch(IllegalStateException ex) {
 			System.out.println("Deu erro na conexao");
-//			con.fecha();
-		} finally {// bloco opcional porém sempre sera executado
-			con.fecha();
+			
 		}
 	}
-
 }
+
+// ----------------------------------------------
+
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//			con.fecha();
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro na conexao");
+//			con.fecha();
+//		} finally {// bloco opcional porém sempre sera executado
+//			con.fecha();
+//		}
+//	}
+//
+//}
